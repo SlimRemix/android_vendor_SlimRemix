d667dc8a25102b76ebe351b98e04b4f04b1151ae
ZION959 <ziontran@gmail.com>
2015-07-10 22:43:56 -0700

    update Viper4Android to lollipop

--- extra/product.mk
+++ extra/product.mk
@@ -14,9 +14,18 @@ PRODUCT_PACKAGES += \
 
 # Viper4Android
 PRODUCT_COPY_FILES += \
-vendor/slimremix/prebuilt/common/etc/viper/ViPER4Android.apk:system/app/ViPER4Android.apk
+vendor/slimremix/prebuilt/common/apk/ViPER4Android/ViPER4Android_FX_A4.x/ViPER4Android_FX_A4.x.apk:system/app/ViPER4Android/ViPER4Android_FX_A4.x.apk
+vendor/slimremix/prebuilt/common/apk/ViPER4Android/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh
+vendor/slimremix/prebuilt/common/apk/ViPER4Android/audio_policy.sh:system/audio_policy.sh
 
 # KCAL - Advanced color control for Qualcomm MDSS 8x26/8974/8084
 PRODUCT_COPY_FILES += \
-vendor/slimremix/prebuilt/common/apk/re.codefi.savoca.kcal.apk:system/app/re.codefi.savoca.kcal.apk
+vendor/slimremix/prebuilt/common/apk/Savoca-Kcal/Savoca-Kcal.apk:system/app/Savoca-Kcal/Savoca-Kcal.apk
 
+# MDNIE-tuner
+PRODUCT_COPY_FILES += \
+vendor/slimremix/prebuilt/common/apk/MDNIE-tuner/MDNIE-tuner.apk:system/app/MDNIE-tuner/MDNIE-tuner.apk
+
+# OpenCamra
+PRODUCT_COPY_FILES += \
+vendor/slimremix/prebuilt/common/apk/OpenCamera/OpenCamera.apk:system/app/OpenCamera/OpenCamera.apk
