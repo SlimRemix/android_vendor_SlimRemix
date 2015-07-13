96aa33079a5933dd0d632ffffec44e3c34608f83
ZION959 <ziontran@gmail.com>
2015-07-12 19:51:49 -0700

    release 5.1.1_R6

--- tools/build-slimremix.sh
+++ tools/build-slimremix.sh
@@ -67,7 +67,7 @@ usage() {
 
 # SlimRemix version
 export SLIMREMIX_VERSION_MAJOR="LP"
-export SLIMREMIX_VERSION_MINOR="MR3"
+export SLIMREMIX_VERSION_MINOR="MR6"
 export SLIMREMIX_VERSION_MAINTENANCE="Unofficial"
 # Acceptable maintenance versions are; Stable, Official, Nightly or Unofficial
 
